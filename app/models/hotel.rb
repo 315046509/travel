a85573c3a2df94a63127a479cf53e745a8ee12a0
shichao <315046509@qq.com>
2014-07-26 13:55:19 +0800

    提交

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,7 +2,7 @@ class Hotel < ActiveRecord::Base
   scope :order_ct_desc, lambda { order("created_at DESC") }
   validates :title, :presence => {:message => "城市名称不能为空"}
 
-  QUESTION_CATEGORYS = [["未分类", 0], ["长春", 1], ["深圳", 2], ["广州", 3], ["三亚", 4], ["马来西亚", 5]]
+  QUESTION_CATEGORYS = [["未分类", 0], ["香港酒店", 1], ["国内酒店", 2], ["国际酒店", 3]]
 
   has_many :occupancy_hotels
 
