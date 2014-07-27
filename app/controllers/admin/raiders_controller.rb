4250177d2bfb397c991308aff96fce12c835335d
shichao <315046509@qq.com>
2014-07-27 14:21:37 +0800

    tijiao

--- app/controllers/admin/raiders_controller.rb
+++ app/controllers/admin/raiders_controller.rb
@@ -12,8 +12,7 @@ class Admin::RaidersController < Admin::MainController
   def create
     @job = Raider.new(
         :title => params[:raider][:title],
-        :description => params[:raider][:description],
-        :city_id => params[:city_id]
+        :description => params[:raider][:description]
     )
     if @job.save
       flash[:note] = "创建成功"
@@ -21,6 +20,25 @@ class Admin::RaidersController < Admin::MainController
     else
       flash[:note] = @job.errors.values.join(";    ")
       redirect_to :back and return
+    end
+  end
+
+  def edit
+    @job = Raider.find(params[:id])
+  end
+
+  def update
+    @job = Raider.find(params[:id])
+    update_category = @job.update_attributes(
+        :title => params[:title],
+        :description => params[:description]
+    )
+    if update_category
+      respond_to do |category|
+        category.html {
+          redirect_to admin_raiders_path and return
+        }
+      end
     end
   end
 
